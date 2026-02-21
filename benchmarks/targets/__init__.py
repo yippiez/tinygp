6fe8ad2c8c9cf38c6911bb0397fdaffffef615f1
Eren <erencemayez@gmail.com>
2026-02-21 21:01:19 +0300

    refactor: move benchmarks into tinygp.benchmark module

--- benchmarks/targets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .keijzer import keijzer_1
-from .nguyen import nguyen_1
-
-__all__ = ["nguyen_1", "keijzer_1"]
--- benchmarks/targets/keijzer.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def keijzer_1(x: np.ndarray) -> np.ndarray:
-    return np.sin(x * x) * np.cos(x) - 1.0
